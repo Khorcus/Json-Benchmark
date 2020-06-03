bcf8cddd087443820b0116c9222ceaf8494964dc
Khorcus <khorcus@gmail.com>
2020-06-03 15:48:21 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <json/json.h>
+#include <nlohmann/json.hpp>
+
+
+#include <iostream>
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
